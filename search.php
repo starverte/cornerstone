94e0a338075cbe3243b802e5ef7f1009fca79eb0
Matt Beall <mbeall@starverte.com>
2014-07-01 13:40:43 -0600

    Clean up spacing

--- search.php
+++ search.php
@@ -7,43 +7,43 @@
 
 get_header(); ?>
 
-		<section id="primary">
-			<div id="content" role="main">
+    <section id="primary">
+      <div id="content" role="main">
 
-			<?php if ( have_posts() ) : ?>
+      <?php if ( have_posts() ) : ?>
 
-				<header class="page-header">
-					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'cornerstone' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-				</header>
+        <header class="page-header">
+          <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'cornerstone' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+        </header>
 
-				<?php cornerstone_content_nav( 'nav-above' ); ?>
+        <?php cornerstone_content_nav( 'nav-above' ); ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+        <?php /* Start the Loop */ ?>
+        <?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content', 'search' ); ?>
+          <?php get_template_part( 'content', 'search' ); ?>
 
-				<?php endwhile; ?>
+        <?php endwhile; ?>
 
-				<?php cornerstone_content_nav( 'nav-below' ); ?>
+        <?php cornerstone_content_nav( 'nav-below' ); ?>
 
-			<?php else : ?>
+      <?php else : ?>
 
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'cornerstone' ); ?></h1>
-					</header><!-- .entry-header -->
+        <article id="post-0" class="post no-results not-found">
+          <header class="entry-header">
+            <h1 class="entry-title"><?php _e( 'Nothing Found', 'cornerstone' ); ?></h1>
+          </header><!-- .entry-header -->
 
-					<div class="entry-content">
-						<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'cornerstone' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
+          <div class="entry-content">
+            <p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'cornerstone' ); ?></p>
+            <?php get_search_form(); ?>
+          </div><!-- .entry-content -->
+        </article><!-- #post-0 -->
 
-			<?php endif; ?>
+      <?php endif; ?>
 
-			</div><!-- #content -->
-		</section><!-- #primary -->
+      </div><!-- #content -->
+    </section><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
